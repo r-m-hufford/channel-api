d55b22649381a676e5b65b180c851de04f32dbb6
Ryan Hufford <ryan@rmhufford.com>
2023-10-21 23:17:01 -0400

    working app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import express, {Request, Response } from 'express';
+import cors from 'cors';
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api', (req: Request, res: Response) => {
+  res.json('contact');
+})
+
+app.listen(3000, ()=> {
+  console.info('listening on port 3000');
+});
 
