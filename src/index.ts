b34a6f23c8148bcd5f1f2ed65070cdcaf8b505b2
Ryan Hufford <ryan@rmhufford.com>
2023-10-22 21:33:33 -0400

    sequalize installed. db connected

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express, {Request, Response } from 'express';
+import { Sequelize } from 'sequelize';
 import cors from 'cors';
+const sequelize = new Sequelize('postgresql://ryanmhufford@localhost:6543/channel');
+try {
+  sequelize.authenticate();
+  console.info('connected to the database')
+} catch(error) {
+  console.error('Unable to connect to database', error);
+}
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
 
 app.get('/api', (req: Request, res: Response) => {
   res.json('contact');
