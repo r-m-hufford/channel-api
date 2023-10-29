618456ec7b3cffaae2ab775f4a5fe46997e037f0
Ryan Hufford <ryan@rmhufford.com>
2023-10-28 22:05:25 -0400

    user CRD

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -11,3 +11,38 @@ userRouter.get('/', async (req, res) => {
     console.error(error);
   }
 });
+
+userRouter.get('/:id', async (req, res) => {
+  try {
+    const users = await User.findOne({
+      where: { id: req.params.id }
+    });
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+userRouter.post('/', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+userRouter.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.destroy({
+      where: { id: req.params.id }
+    });
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+  }
+});
