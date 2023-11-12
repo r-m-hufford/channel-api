918c2620eaa4136291456a7b1808c915c4d37897
Ryan Hufford <ryan@rmhufford.com>
2023-11-12 14:08:51 -0500

    follow service

--- src/routes/follows.ts
+++ src/routes/follows.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import { getFollowers } from '../services/follow-service';
 
 export const followsRouter = express.Router();
 
-followsRouter.get('/', (req, res) => {
-  res.send('Hello, world!');
+followsRouter.get('/', async (req, res) => {
+  try {
+    const followers = await getFollowers(req.params.userId);
+    res.status(200).json(followers);
+  } catch (error) {
+    
+  }
 });
 
 followsRouter.post('/', (req, res) => {
